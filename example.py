edd3a24a9d1d82a840cc944dfa551842c1840496
Dmitrijs Trizna <dtrizna@DiMac.local>
2020-09-22 12:29:22 +0300

    optimization

--- example.py
+++ example.py
@@ -7,6 +7,12 @@ p = Parser(debug=True, verbose=True)
 
 counted = p.parse(data)
 
-print(len(counted))
-print(counted.most_common(10))
+print("\n", "Total unique elements found: ", len(counted))
+print(counted.most_common(5))
+"""
+% python3 example.py
+[!] Parsing in process.. 12606\12607
+Total unique elements found: 10372
+[('find', 7846), ('|', 6487), ('.', 3775), ('-name', 3616), ('-type', 3403)]
+"""
 #import pdb; pdb.set_trace()
