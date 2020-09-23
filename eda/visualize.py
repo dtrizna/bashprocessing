2cb1b4c5839f9da0ac13798ecff9886c08fc6d10
Dmitrijs Trizna <dtrizna@DiMac.local>
2020-09-23 12:51:59 +0300

    visualisation

--- eda/visualize.py
+++ eda/visualize.py
@@ -1,21 +1,31 @@
 import seaborn as sns
 import matplotlib.pyplot as plt
-import string
 import numpy as np
-
-from bashprocessing import Parser
+import string
+import sys
 
 from collections import Counter
+from os import path
 
 from wordcloud import WordCloud, ImageColorGenerator
 from PIL import Image
-from os import path
 
-with open('all.cm') as f:
-    data = f.readlines()
+scriptpath = ".."
+sys.path.append(path.abspath(scriptpath))
+scriptpath = "."
+sys.path.append(path.abspath(scriptpath))
+
+from bashprocessing import Parser
+
+try:
+    with open('all.cm') as f:
+        data = f.readlines()
+except FileNotFoundError:
+    with open('../all.cm') as f:
+        data = f.readlines()
 
 p = Parser(verbose=True)
-counter, corpus = p.parse(data[0:4000])
+counter, corpus = p.parse(data)
 
 bash_stopwords = [key for key in counter.keys() if all(key.isdigit() or len(key) < 3 or j in string.punctuation for j in key)]
 clean_dict = {key:counter[key] for key in counter.keys() if not all(key.isdigit() or len(key) < 3 or j in string.punctuation for j in key)}
@@ -25,7 +35,7 @@ clean_dict = {key:counter[key] for key in counter.keys() if not all(key.isdigit(
 
 d = path.dirname(__file__) if "__file__" in locals() else os.getcwd()
 
-tuxmask = np.array(Image.open(path.join(d, "img/Tux.png")))#.convert("1")).astype(int)
+tuxmask = np.array(Image.open(path.join(d, "../img/Tux.png")))#.convert("1")).astype(int)
 img_color = ImageColorGenerator(tuxmask)
 
 wc = WordCloud(background_color="white", \
@@ -41,7 +51,7 @@ plt.figure(figsize=(12,8))
 plt.imshow(wc, interpolation='bilinear')
 plt.axis("off")
 plt.show()
-#plt.savefig('img/wordcloud.png', bbox_inches='tight')
+#plt.savefig('../img/wordcloud.png', bbox_inches='tight')
 #plt.close()
 
 
@@ -53,7 +63,6 @@ sns.set_theme(style="whitegrid")
 sns.set(font_scale=1.4)
 plt.figure(figsize=(14,6))
 bars = sns.barplot(x=list(X), y=list(Y), palette="Blues_d")
-bars.get_figure().savefig("img/absolute_element_counts.png")
+#bars.get_figure().savefig("../img/absolute_element_counts.png")
 plt.show()
-#plt.savefig('img/absolute_element_counts.png', bbox_inches='tight')
 #plt.close()
